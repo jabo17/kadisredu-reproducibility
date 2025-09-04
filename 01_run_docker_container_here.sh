3628b55a47dc27fa63037d52c451df1da167f7f4
Jannick <jannick.borowitz@gmail.com>
2025-09-04 18:12:10 +0200

    update

--- 01_run_docker_container_here.sh
+++ 01_run_docker_container_here.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
+# Run the docker container
+# NOTE: if you do not have write access while running the docker contaienr to to $(pwd),
+# pass -u '${UID}:${GID}' in addition
 docker run -it --rm -v $(pwd):/home/kadisredu --shm-size=4gb ghcr.io/jabo17/kadisredu-reproducibility:main
