48bff5c789e8364e2ff3b7cd5c9ad13f16c1d4b3
Jannick Borowitz <37897081+jabo17@users.noreply.github.com>
2025-10-08 08:04:55 +0200

    Update 01_run_kadisredu_weak.sh

--- experiments/01_run_kadisredu_weak.sh
+++ experiments/01_run_kadisredu_weak.sh
@@ -7,9 +7,11 @@ mkdir -p experiment_out
 
 pipenv install
 
+(( cores=$(nproc)/2 )) # assumes nproc returns hyperthreads
+
 pipenv run ../kaval/run-experiments.py 01_kadisredu_weak \
     --search-dirs ./suites \
     --machine shared \
-    --max-cores $(nproc) \
+    --max-cores $cores \
     --output-dir ./experiment_out/kadisredu_weak/output \
     --experiment-data-dir ./experiment_out/
