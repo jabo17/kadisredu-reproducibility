7cd9b2419410aa31747b93ecdc8884de8bd2b6ff
Jannick Borowitz <37897081+jabo17@users.noreply.github.com>
2025-10-08 08:05:11 +0200

    Update 02_run_htwis.sh

--- experiments/02_run_htwis.sh
+++ experiments/02_run_htwis.sh
@@ -7,10 +7,12 @@ mkdir -p experiment_out
 
 pipenv install
 
+(( cores=$(nproc)/2 )) # assumes nproc returns hyperthreads
+
 pipenv run ../kaval/run-experiments.py 02_htwis \
     --search-dirs ./suites \
     --machine shared \
-    --max-cores $(nproc) \
+    --max-cores $cores \
     --output-dir ./experiment_out/htwis/output \
     --experiment-data-dir ./experiment_out/ \
     --omit-output-path
