2de7d4d73d59c548201b8cb19c2493653b9289a5
Jannick <jannick.borowitz@gmail.com>
2025-10-08 14:16:38 +0200

    added zenodo link

--- 02_fetch_algorithms.sh
+++ 02_fetch_algorithms.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-# The permanently archived code is available at https://zenodo.org/records/17174408
+# The permanently archived code is available at https://doi.org/10.5281/zenodo.17174407
+# The permanent release is shiped with all the git submodules and FetchContent dependencies.
 git clone https://github.com/jabo17/kadisredu.git kadisredu
 cd kadisredu
 git submodule update --init --recursive
