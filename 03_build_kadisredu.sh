9aedcd094fde550be7eac001798786b996b9b8b8
Jannick Borowitz <37897081+jabo17@users.noreply.github.com>
2025-10-08 16:14:19 +0200

    Update 03_build_kadisredu.sh

--- 03_build_kadisredu.sh
+++ 03_build_kadisredu.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+offline="FALSE"
+if [ "$1" == "offline" ]; then
+  offline="TRUE"
+fi
+
 cd kadisredu
 mkdir -p build
-cmake --preset=Release -S . -B build
+
+cmake --preset=Release -S . -B build -DKADISREDU_BUILD_OFFLINE="${offline}"
 cmake --build build --target kadisredu_app --parallel 
 cd ..
